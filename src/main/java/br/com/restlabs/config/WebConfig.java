3cec3ff844dd366197927a6d2fae62abe0282dc5
wagnerdsodre <wagnerdsodre@gmail.com>
2023-01-22 20:31:56 -0300

    Content negotiation header parameter

--- src/main/java/br/com/restlabs/config/WebConfig.java
+++ src/main/java/br/com/restlabs/config/WebConfig.java
@@ -9,9 +9,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
 
-               configurer.favorParameter(true)
-               .parameterName("mediaType")
-               .ignoreAcceptHeader(true)
+               configurer.favorParameter(false)
+               .ignoreAcceptHeader(false)
                .useRegisteredExtensionsOnly(false)
                .defaultContentType(MediaType.APPLICATION_JSON)
                .mediaType("json",MediaType.APPLICATION_JSON)
